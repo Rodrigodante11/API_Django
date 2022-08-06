fd592c0e9f10360290f100d7e5b0ee70785c6638
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-08-06 18:45:46 -0300

    adicionando serializer as dados do model/banco

--- escola/views.py
+++ escola/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 
 # Create your views here.
 def alunos(request):
-    if request.method =="GET":
+    if request.method == "GET":
         aluno = {
             'id': 1,
             'nome': 'Rodrigo'
